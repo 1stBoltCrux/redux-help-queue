fb00f9b1bd489ce3427451936590c6e2a0088397
matt dowdney <theofficialmatt@gmail.com>
2018-06-30 14:19:16 -0700

    add new ticket control and confirmationquestion components, state

--- src/components/Ticket.jsx
+++ src/components/Ticket.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-function Ticket(){
+function Ticket(props){
   return (
     <div>
       <style global jsx>{`
@@ -9,8 +9,8 @@ function Ticket(){
             background-color: red;
           }
         `}</style>
-      <h3>3A - Thato & Haley</h3>
-      <p><em>Firebase will not save record!</em></p>
+        <h3>{props.location} - {props.names}</h3>
+         <p><em>{props.issue}</em></p>
       <hr/>
     </div>
   );
